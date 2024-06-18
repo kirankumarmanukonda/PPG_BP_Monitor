c1792ace1fda03758ba76264b93fa0b6f4f4c235
kirankumar <kiran@ayatidevices.com>
2024-06-18 15:10:36 +0530

    Read ppg and show as graph on tft

--- include/main.h
+++ include/main.h
@@ -13,16 +13,6 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 /*
  * ESP32 with the ILI9341 display
  * 
- * Common colors:
- * BLACK    0x0000
- * BLUE     0x001F
- * RED      0xF800
- * GREEN    0x07E0
- * CYAN     0x07FF
- * MAGENTA  0xF81F
- * YELLOW   0xFFE0
- * WHITE    0xFFFF
-
  * Pin Configuration
  * LED =   32
  * SCK =   18
@@ -32,8 +22,16 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
  * CS =    15
  * GND =   GND
  * VCC =   5V or 3.3V (the display has it's own 3.3V regulator)
-
-
-
-
+ *
+ *  Common colors:
+ * BLACK    0x0000
+ * BLUE     0x001F
+ * RED      0xF800
+ * GREEN    0x07E0
+ * CYAN     0x07FF
+ * MAGENTA  0xF81F
+ * YELLOW   0xFFE0
+ * WHITE    0xFFFF
+ * 
+ * 
 */
--- src/main.cpp
+++ src/main.cpp
@@ -5,59 +5,87 @@
 #define SCREEN_HEIGHT 240
 
 // Graph dimensions and position
-#define GRAPH_WIDTH 300
-#define GRAPH_HEIGHT 200
-#define GRAPH_X 10
-#define GRAPH_Y 20
+#define GRAPH_X 50
+#define GRAPH_Y 100
+#define GRAPH_WIDTH 270
+#define GRAPH_HEIGHT 140
 
-// Define the maximum number of data points to display on the screen
+// Define the maximum number of ppg_data points to display on the screen
 #define MAX_POINTS GRAPH_WIDTH
 
-int data[MAX_POINTS]; // Array to hold data points
+// Pressure Reading box
+#define BP_BOX_X 0
+#define BP_BOX_Y 0
+
+int ppg_data[MAX_POINTS]; // Array to hold ppg_data points
 
 // Function declarations
 void drawAxes();
 void plotGraph(int);
-
+void initialize_plot();
+int newValue;
 void setup() 
 {
+  Serial.begin(115200);
+
+  //Turn on the LED
   pinMode(32, OUTPUT);
   digitalWrite(32, 1);
+
+  // For PPG sensor
+  pinMode(14, OUTPUT);
+  digitalWrite(14, 0);
+
+  pinMode(27, OUTPUT);
+  digitalWrite(27, 1);
+  
+  pinMode(26, INPUT);
+
+  initialize_plot();
 
   //Initialize the display
   tft.begin();
   tft.setRotation(1); // Set display rotation
   tft.fillScreen(ILI9341_BLACK); // Fill screen with black
   
+  // Draw the BP value and a Box
+  tft.setCursor(BP_BOX_X, BP_BOX_Y);  // Set position (x,y)
+  tft.setTextColor(0x07FF);  // Set color of text. First is the color of text and after is color of background
+  tft.setTextSize(2);  // Set text size. Goes from 0 (the smallest) to 20 (very big)
+  tft.println(" BP ");
+  tft.drawRoundRect(BP_BOX_X, BP_BOX_Y + 20, 50, 50, 8, 0x07FF);
+
+  tft.setCursor(BP_BOX_X + 10, BP_BOX_Y + 40);
+  tft.setTextSize(2); 
+  tft.println("900");
+  
   // Draw initial axes
-  drawAxes();
+  //drawAxes();
 }
 
 void loop() 
 {
-  static float angle = 0.0; // Initial angle for sine wave
-
+  //static float angle = 90.0; // Initial angle for sine wave
   // Generate a new sine wave value
-  int newValue = GRAPH_Y + GRAPH_HEIGHT / 2 + (sin(angle) * (GRAPH_HEIGHT / 2)); // Sine wave mapped to graph height
-  angle += 0.1; // Increment angle for next point
+  //int newValue = GRAPH_Y + GRAPH_HEIGHT / 2 + (sin(angle) * (GRAPH_HEIGHT / 2)); // Sine wave mapped to graph height
+  //angle += 0.1; // Increment angle for next point
+
+  newValue = map(analogRead(26), 0, 4096, GRAPH_HEIGHT + GRAPH_Y, GRAPH_Y);
 
   // Erase the previous graph by drawing it in black
   plotGraph(1);
 
-  // Shift the previous data points to the left
+  // Shift the previous ppg_data points to the left
   for (int i = 0; i < MAX_POINTS - 1; i++) 
-  {
-    data[i] = data[i + 1];
-  }
+    ppg_data[i] = ppg_data[i + 1];
 
   // Add the new value to the array
-  data[MAX_POINTS - 1] = newValue;
+  ppg_data[MAX_POINTS - 1] = newValue;
 
   // Plot the new graph
   plotGraph(0);
+  
 
-  // Delay for demonstration (adjust based on your requirements)
-  delay(50);
 }
 
 void drawAxes() 
@@ -71,13 +99,21 @@ void plotGraph(int clear)
   for (int i = 0; i < MAX_POINTS - 1; i++) 
   {
     int x0 = GRAPH_X + i;
-    int y0 = data[i];
+    int y0 = ppg_data[i];
     int x1 = GRAPH_X + (i + 1);
-    int y1 = data[i + 1];
-
+    int y1 = ppg_data[i + 1];
+    
     if(clear)
-        tft.drawLine(x0, y0, x1, y1, ILI9341_BLACK); // Erase the previous graph with black color
+      tft.drawLine(x0, y0, x1, y1, ILI9341_BLACK); // Erase the previous graph with black color
     else
-    tft.drawLine(x0, y0, x1, y1, ILI9341_RED); // Red color
+      tft.drawLine(x0, y0, x1, y1, ILI9341_RED); // Red color
+  }
+}
+
+void initialize_plot()
+{
+  for(int i = 0; i < MAX_POINTS; i++)
+  {
+    ppg_data[i] = GRAPH_HEIGHT + GRAPH_Y;
   }
 }
